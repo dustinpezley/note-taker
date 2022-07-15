564e902e2e4a01810a42e8ca8e047d387ffc1ced
dustinpezley <dustinpezley@gmail.com>
2022-07-14 22:41:48 -0500

    added Note object and apiRoutes for get and post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,10 +3,29 @@ const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 const { notes } = require('../db/db.json');
+const Note = require('../lib/Note');
 
 router.get('/notes', (req, res) => {
-  let results = notes;
-  res.json(results);
+  if(req.body) {
+    res.sendFile(path.join(__dirname,notes));
+  } else {
+    res.json('Unable to retrieve data.');
+  }
+});
+
+router.post('/notes', (req,res) => {
+  let noteArray = fs.readFile(notes, 'utf-8', (data,err) => {
+    if(err) {
+      res.json(`There was an error with your request: ${err}.`);
+    }
+    return JSON.parse(data);
+  })
+
+  let newNote = new Note(req.body.title, req.body.text);
+
+  noteArray = [...noteArray, newNote];
+
+  fs.writeFile(notes, JSON.stringify(noteArray));
 })
 
 module.exports = router;
