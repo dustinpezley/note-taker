1dcbd6690360ca996d1cf2677fdbd9f6e223920f
dustinpezley <dustinpezley@gmail.com>
2022-07-17 21:33:37 -0500

    post and delete functionality working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,28 +4,31 @@ const fs = require('fs');
 const path = require('path');
 const { notes } = require('../db/db.json');
 const Note = require('../lib/Note');
+const { createNewNote } = require('../lib/noteHandler');
 
 router.get('/notes', (req, res) => {
   if(req.body) {
-    res.sendFile(path.join(__dirname,notes));
+    res.sendFile(path.join(__dirname,'../db/db.json'));
   } else {
     res.json('Unable to retrieve data.');
   }
 });
 
 router.post('/notes', (req,res) => {
-  let noteArray = fs.readFile(notes, 'utf-8', (data,err) => {
-    if(err) {
-      res.json(`There was an error with your request: ${err}.`);
-    }
-    return JSON.parse(data);
-  })
+  let noteArray = JSON.parse(fs.readFileSync('db/db.json','utf-8'));
+  if(!req.body) {
+    res.status(400).send('Improper request.')
+  } else {
+    let newNote = new Note(req.body.title, req.body.text);
+    const note = createNewNote(newNote, noteArray);
+    return note;
+  }
+})
 
-  let newNote = new Note(req.body.title, req.body.text);
-
-  noteArray = [...noteArray, newNote];
-
-  fs.writeFile(notes, JSON.stringify(noteArray));
+router.delete('/notes/:id', (req,res) => {
+  let noteArray = JSON.parse(fs.readFileSync('db/db.json','utf-8'));
+  const noteRemoved = noteArray.filter(note => note.id !== req.params.id);
+  fs.writeFileSync(path.join(__dirname, '../db/db.json'),JSON.stringify(noteRemoved, null, 2))
 })
 
 module.exports = router;
