813f023e2effbf255c345086b96aaf9344d71607
dustinpezley <dustinpezley@gmail.com>
2022-07-19 21:07:27 -0500

    updated post and delete to refresh page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,6 +9,8 @@ const { createNewNote } = require('../lib/noteHandler');
 router.get('/notes', (req, res) => {
   if(req.body) {
     res.sendFile(path.join(__dirname,'../db/db.json'));
+    // res.json(getNotes())
+    // .then((note) => {return res.json(note)});
   } else {
     res.json('Unable to retrieve data.');
   }
@@ -21,14 +23,15 @@ router.post('/notes', (req,res) => {
   } else {
     let newNote = new Note(req.body.title, req.body.text);
     const note = createNewNote(newNote, noteArray);
-    return note;
+    res.json(note);
   }
 })
 
 router.delete('/notes/:id', (req,res) => {
   let noteArray = JSON.parse(fs.readFileSync('db/db.json','utf-8'));
   const noteRemoved = noteArray.filter(note => note.id !== req.params.id);
-  fs.writeFileSync(path.join(__dirname, '../db/db.json'),JSON.stringify(noteRemoved, null, 2))
+  fs.writeFileSync(path.join(__dirname, '../db/db.json'),JSON.stringify(noteRemoved, null, 2));
+  res.json(noteRemoved);
 })
 
 module.exports = router;
